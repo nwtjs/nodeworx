73f994e1d21d2d2d3134c2e72d2d470f7179e62f
Kevin Grandon <kevingrandon@yahoo.com>
2011-10-01 21:13:46 +0000

    Comment.

--- config.connections.js
+++ config.connections.js
@@ -3,6 +3,9 @@
 	// The port to listen on for connections
 	root.port = 80;
 
+	// A mapping of hostname to site definitions
+	// Each site definition should have a folder which corresponds to a 
+	// top level directory inside of the NWT folder.
 	root.connections = {
 		'example' : {
 			folder : 'example'
