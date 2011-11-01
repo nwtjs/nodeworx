53c57c4ca869bfb17169eca9d3f1964648aae733
Kevin Grandon <kevingrandon@yahoo.com>
2011-11-01 07:18:31 +0000

    Update docs.

--- example/views/docs/basics_installation.js
+++ example/views/docs/basics_installation.js
@@ -3,7 +3,13 @@
 
 	content: Html.h1('Installation') +
 		Html.p('1 - Recommend install is with NPM: $ npm install nwt') +
-		Html.p('2 - Note: due to the npm install, the entire framework will now reside within node_modules/nwt. We recommend moving the framework, or creating a symlink for easier access: ln -s node_modules/nwt nwt. This will create a nwt symlink for you as a shortcut. You should now be able to type: cd nwt to enter the framework directory.') +
-		Html.p('3 - Run the server: $ node _core/server.js') +
-		Html.p('4 - Navigate to your hostname (defaults to port 80). This will launch the example app. ')
+		Html.p('2 - Note: due to the npm install, the entire framework will now reside within node_modules/nwt. The easiest way to work with NWT is by leaving the framework in the installed location, and to configure your sites using the command line tool.') +
+		Html.p('3 - Initialze the NWT environment: $ node node_modules/nwt/_core/cli/tool.js init') +
+		Html.p('4 - Run the server: $ node server.js') +
+		Html.p('5 - Navigate to your hostname (defaults to port 80). This will launch the example app. ') +
+		Docs.example(
+			"Creating a site",
+			"The easiest way to generate a NWT site is by using the included command line tool. Call the create method with the hostname that points to your host. For example, we can replace &lt;hostname&gt; with nwtjs.org for this site.",
+			'node node_modules/nwt/_core/cli/tool.js create <hostname>', true
+		)
 }
--- example/views/docs/basics_structure.js
+++ example/views/docs/basics_structure.js
@@ -2,9 +2,7 @@
 	pageTitle: 'Directory Structure',
 
 	content: Html.h1('Directory Structure') +
-	Html.p('Within the root directory of a stock install, you will find two folders. One named "_core", this contains core NWT classes and libraries, and one named "example". The "example" folder contains a fully functioning application.') + 
-	Html.h3('Creating Applications') +
-	Html.p('Each application resides in a folder directly under the root folder. Once you have created the folder, you should open nwt/config.connections.js, and create an entry there. At the most basic form, this file maps a hostname to an application folder. Each application generally has a folders named "views", and a folder named "models". Take a look around the example application to become familiar with this directory structure.') +
+	Html.p('Immediately after install the root directory should only contain the node_modules folder. All NWT code is located inside of the node_modules/nwt folder, you should not need to directly access this folder.') +
 	Html.h3('Static Resources') +
 	Html.p('If you are serving static resources from your NWT installation, we recommend that you place the resources specific to each application inside of that folder. There is no enforced convention, but we recommend placing CSS files inside of nwt/application/css/***.css and images at: nwt/application/img/***.***.')
 }
